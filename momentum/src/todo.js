0c64ad1ab2661201e14c293827e26b4f00824bfc
00-dh <kimdh081599@gmail.com>
2022-08-14 15:54:57 +0900

    update todo list

--- momentum/src/todo.js
+++ momentum/src/todo.js
@@ -1,11 +1,31 @@
 const toDoForm = document.querySelector(".todo_form");
 const toDoInput = document.querySelector(".input_todo");
 const toDoList = document.querySelector(".todo_list");
+const toDoBtn = document.querySelector(".todo_button");
+const emptyMessage = document.querySelector(".empty");
+
+function todoBtnClick() {
+  toDoList.classList.toggle("hidden");
+}
+
+toDoBtn.addEventListener("click", todoBtnClick);
 
 let toDos = [];
 
+if (toDos.length === 0) {
+  emptyMessage.innerText = "Write your todo";
+} else {
+  emptyMessage.innerText = "Your Todo List";
+}
+
 function saveToDos() {
   localStorage.setItem("todos", JSON.stringify(toDos));
+
+  if (toDos.length === 0) {
+    emptyMessage.innerText = "Write your todo";
+  } else {
+    emptyMessage.innerText = "Your Todo List";
+  }
 }
 
 function deleteTodo(event) {
@@ -23,8 +43,8 @@ function paintToDo(newTodo) {
   const button = document.createElement("button");
   button.innerText = "❌";
   button.addEventListener("click", deleteTodo);
-  li.appendChild(button);
   li.appendChild(span);
+  li.appendChild(button);
   toDoList.appendChild(li);
 }
 
